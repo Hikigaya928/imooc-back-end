5d93569d31bca36658b8b0f9e841523319321bf4
Hikigaya928 <470564854@qq.com>
2023-11-14 17:08:07 +0800

    登录api开发

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = require('./router')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // 创建 express 应用
 const app = express()
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/', router)
 
 // 使 express 监听 5000 端口号发起的 http 请求
